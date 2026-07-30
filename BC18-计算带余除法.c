20cd433a661c37e9f47f4941fa1977b881be93a5
Languors <201385097@qq.com>
2026-07-31 04:58:02 +0800

    Create BC18-计算带余除法.c

---
+++ BC18-计算带余除法.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    scanf("%d %d", &a, &b);
+    int m = a/b;
+    int n = a%b;
+    printf("%d %d\n", m, n);
+    
+    return 0;
+}
